9244143412c603ac72bbb9b572caf4c2c1412d47
Benjamin Glaspie <02Benjamin.Glaspie@gulllakecs.org>
2019-02-11 06:54:30 -0800

    Add a greetings function

---
+++ Functions-GIT.py
@@ -0,0 +1,10 @@
+# 4.13.3: Greetings
+# Ben Glaspie
+# 2/6/19
+
+name = input("what is your name: ")
+
+def greeting():
+    print("Hi there" + name + "!")
+    print("Nice to meet you!")
+greeting()
