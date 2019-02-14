c2018451aca57cebd90bbf25b6c42034c2248a28
Benjamin Glaspie <02Benjamin.Glaspie@gulllakecs.org>
2019-02-14 06:33:44 -0800

    Add Functions and Variable

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -2,9 +2,26 @@
 # Ben Glaspie
 # 2/6/19
 
+'''
 name = input("what is your name: ")
 
 def greeting():
     print("Hi there" + name + "!")
     print("Nice to meet you!")
+
 greeting()
+
+'''
+# 4.13.4: Functions and variables
+# Ben Glaspie
+# 2.14.19
+
+x = 406
+
+def print_something():
+    x = 13
+    print(x)
+
+
+print_something()
+print(x)
