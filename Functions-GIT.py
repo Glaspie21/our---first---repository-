5528a50a0217689ec8075dd5f3a1afbb7415be81
Benjamin Glaspie <02Benjamin.Glaspie@gulllakecs.org>
2019-02-27 07:00:32 -0800

    Add functions and vairables part 3

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -2,7 +2,7 @@
 # Ben Glaspie
 # 2/6/19
 
-'''
+
 name = input("what is your name: ")
 
 def greeting():
@@ -11,7 +11,7 @@ def greeting():
 
 greeting()
 
-'''
+
 # 4.13.4: Functions and variables
 # Ben Glaspie
 # 2.14.19
@@ -25,3 +25,24 @@ def print_something():
 
 print_something()
 print(x)
+
+
+# 4.13.5: functions and Varibles part 2
+# Ben Glaspie
+# 1.13.19
+
+my_variable = 3.6745
+
+def something()
+    
+
+
+#4.13.6: Functions and variables, part 3
+
+
+def print_number(x):
+    print(str(x))
+
+Print_number(12)
+print_number('Hello World')
+
